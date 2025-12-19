baeea5c7b31cf4a3ad3e9dfcd71d509f8ecc0d38
Andries Liebenberg <create@an3s.info>
2025-12-19 10:37:41 +0000

    6000-firebase-studio-1765529117518.cluster-fbfjltn375c6wqxlhoehbz44sk.cl

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,10 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   experimental: {
+    serverActions: {
+      allowedOrigins: ["*.google.com", "*.firebase.app", "*.cloud.run", "*.cloud.google.com", "localhost:6000", "6000-firebase-studio-1765529117518.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev"]
+    }
   },
-  allowedDevOrigins: ["https://6000-firebase-studio-1765529117518.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev"],
 };
 
 export default nextConfig;
