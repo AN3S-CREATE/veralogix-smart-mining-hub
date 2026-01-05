8d0c1d01f329e843fca83120996e61b28411494e
Andries Liebenberg <create@an3s.info>
2026-01-05 14:31:18 +0000

    These are not my pictures. I've also got the link. If they see this, the

--- src/lib/placeholder-images.ts
+++ src/lib/placeholder-images.ts
@@ -12,7 +12,7 @@ export const PlaceHolderImages: ImagePlaceholder[] = [
     {
         id: 'login-logo',
         description: 'The main logo displayed on the login page.',
-        imageUrl: 'https://picsum.photos/seed/veralogix-logo/420/69',
+        imageUrl: '/veralogix-logo.png',
         imageHint: 'company logo',
         width: 420,
         height: 69
@@ -20,7 +20,7 @@ export const PlaceHolderImages: ImagePlaceholder[] = [
     {
         id: 'sidebar-logo',
         description: 'The Veralogix logo displayed in the main application sidebar.',
-        imageUrl: 'https://picsum.photos/seed/sidebar-logo/192/31',
+        imageUrl: '/veralogix-logo.png',
         imageHint: 'company logo',
         width: 192,
         height: 31,
@@ -28,7 +28,7 @@ export const PlaceHolderImages: ImagePlaceholder[] = [
      {
         id: 'login-background',
         description: 'A background image of professional mining equipment.',
-        imageUrl: 'https://picsum.photos/seed/mining-background/1920/1080',
+        imageUrl: '/background.png',
         imageHint: 'mining equipment'
     }
 ];
