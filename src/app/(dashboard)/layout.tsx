4d27a6767602513465d168685f5d8188b8456c14
Andries Liebenberg <create@an3s.info>
2026-01-07 16:46:33 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: useFirestore mus

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,11 @@
+
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/global/app-sidebar";
 import { Header } from "@/components/global/header";
 import { OfflineBanner } from "@/components/shared/offline-banner";
 import { PageLoader } from "@/components/global/page-loader";
 import { Suspense } from "react";
+import { FirebaseClientProvider } from "@/firebase";
 
 export default function DashboardLayout({
   children,
@@ -11,18 +13,20 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <SidebarProvider>
-      <PageLoader />
-      <AppSidebar />
-      <SidebarInset>
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <main className="flex-1 p-4 sm:p-6 overflow-auto">
-            <OfflineBanner />
-            {children}
-          </main>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+    <FirebaseClientProvider>
+      <SidebarProvider>
+        <PageLoader />
+        <AppSidebar />
+        <SidebarInset>
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-1 p-4 sm:p-6 overflow-auto">
+              <OfflineBanner />
+              {children}
+            </main>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </FirebaseClientProvider>
   );
 }
